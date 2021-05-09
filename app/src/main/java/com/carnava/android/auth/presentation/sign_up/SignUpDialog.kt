d5b2d17dd87897b0b3984739adc4e6b51eebe630
Jamal <aliev.djamal.2000@gmail.com>
2021-05-09 11:35:56 +0300

    "SignUp" create layout

---
+++ app/src/main/java/com/carnava/android/auth/presentation/sign_up/SignUpDialog.kt
@@ -0,0 +1,7 @@
+package com.carnava.android.auth.presentation.sign_up
+
+import androidx.fragment.app.DialogFragment
+import com.carnava.android.R
+
+class SignUpDialog : DialogFragment(R.layout.dialog_sign_up) {
+}
--- app/src/main/java/com/carnava/android/auth/presentation/sign_up/SignUpFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.carnava.android.auth.presentation.sign_up
-
-import com.carnava.android.core.ui.BaseFragment
-
-class SignUpFragment : BaseFragment() {
-}
