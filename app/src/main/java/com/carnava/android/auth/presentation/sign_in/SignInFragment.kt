040a5337270ed5115181c5d3424c4038ad8bbbdb
Jamal <aliev.djamal.2000@gmail.com>
2021-05-09 10:34:22 +0300

    "SignIn" create layout

--- app/src/main/java/com/carnava/android/auth/presentation/sign_in/SignInFragment.kt
+++ app/src/main/java/com/carnava/android/auth/presentation/sign_in/SignInFragment.kt
@@ -1,6 +1,7 @@
 package com.carnava.android.auth.presentation.sign_in
 
+import com.carnava.android.R
 import com.carnava.android.core.ui.BaseFragment
 
-class SignInFragment : BaseFragment() {
+class SignInFragment : BaseFragment(R.layout.fragment_sign_in) {
 }
