158d411b0d10aff83861c506120cfa6f3097673e
Jamal <aliev.djamal.2000@gmail.com>
2021-05-09 11:16:05 +0300

    "SignIn" fix bugs and change design

--- app/src/main/java/com/carnava/android/core/navigation/AppNavigationFactory.kt
+++ app/src/main/java/com/carnava/android/core/navigation/AppNavigationFactory.kt
@@ -1,6 +1,7 @@
 package com.carnava.android.core.navigation
 
 import com.carnava.android.app.presentation.AppActivity
+import com.carnava.android.auth.presentation.sign_in.SignInFragment
 import com.carnava.android.core.navigation.fragment_controllers.NavigationControllerFragment
 import com.carnava.android.core.navigation.fragment_controllers.TabNavigationControllerFragment
 import me.aartikov.alligator.navigationfactories.RegistryNavigationFactory
@@ -8,7 +9,15 @@ import me.aartikov.alligator.navigationfactories.RegistryNavigationFactory
 class AppNavigationFactory : RegistryNavigationFactory() {
     init {
         registerActivity(Screens.Application::class.java, AppActivity::class.java)
-        registerFragment(Screens.TabNavigationController::class.java, TabNavigationControllerFragment::class.java)
-        registerFragment(Screens.NavigationController::class.java, NavigationControllerFragment::class.java)
+        registerFragment(
+            Screens.TabNavigationController::class.java,
+            TabNavigationControllerFragment::class.java
+        )
+        registerFragment(
+            Screens.NavigationController::class.java,
+            NavigationControllerFragment::class.java
+        )
+
+        registerFragment(Screens.SignIn::class.java, SignInFragment::class.java)
     }
 }
